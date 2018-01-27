05f184ec38e1435c58d6cfc033804f3adb07def5
J.A. Torres-Matallana <arturo.torres@list.lu>
2018-01-27 14:52:31 +0100

    initial_commit

---
+++ source/stUPscales_102/R/Class-setup.R
@@ -0,0 +1,21 @@
+setup <- setClass("setup", 
+         slots = c(id        = "character",
+                   nsim      = "numeric", 
+                   seed      = "numeric",
+                   mcCores   = "numeric",
+                   ts.input  = "data.frame",
+                   rng       = "list",
+                   ar.model  = "list",
+                   var.model = "list",
+                   folderOutput = "character"),
+                   
+         prototype = list(id        = "MC_sim_1",
+                          nsim      = 1,
+                          seed      = 0.7010607,
+                          mcCores   = 1,
+                          ts.input  = NULL,
+                          rng       = NULL,
+                          ar.model  = list(NULL),
+                          var.model = list(NULL),
+                          folderOutput = "~/R-output ")
+         )
